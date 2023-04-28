c0d195cee97c9b74bfc134cf3681d990e2fd0bec
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-04-29 00:44:32 +0200

    set up server

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import 'express-async-errors';
+import mongoose from "mongoose";
+
+const app = express();
+
+// GLOBAL MIDDLEWARES
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json("Hello world");
+});
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/twitter");
+  console.log("Connected to Database");
+
+  app.listen(3000, () =>
+    console.log(
+      "Server is running on: http://localhost:3000"
+    )
+  );
+}
+
+main().catch(console.error);
